22f96c18cd5ae1ef1e3f51b118819873cd1ec0c9
Rafael Luiz <rafacoder2020@outlook.com>
2021-04-21 18:46:39 -0300

    listagem dos posts sem estilização

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Posts: React.FC = () => <h1>Posts</h1>;
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const Posts: React.FC = () => {
+  const [posts, setPost] = useState<Post[]>([]);
+
+  useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+        setPost(response.data);
+
+        console.log(posts)
+      });
+
+  }, [posts]);
+
+  return(
+    <>
+      <ul>
+        {posts.map(post =>( 
+            <li key={post.id}>{post.title}</li>
+          )
+        )}
+      </ul>
+    </>
+  );
+
+}
 
 export default Posts;
